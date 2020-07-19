3a82ad71c7458f2180bf8b31335f4235bbe3c597
boaztanui <boaztanui058@gmail.com>
2020-07-19 09:33:09 +0300

    created projects with create react app and added projects files for homepage

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.component';
+import './homepage.styles.scss';
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+
+      
+    </div>
+  );
+}
+
+export default App;
