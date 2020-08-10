6a380e3a1151b0c70779cbdc83f372a670e7c774
boaztanui <BOAZTANUI058@.COM>
2020-08-10 10:16:16 +0300

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,7 +9,7 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
+const CartIcon = ({ toggleCartHidden }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
         <span className='item-count'> 0 </span>
